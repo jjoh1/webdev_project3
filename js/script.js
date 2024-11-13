e3e9888ab3e07b750d2e3454d28f110d77b822d1
jjoh1 <jjoh1@cps.edu>
2024-11-12 23:44:23 -0600

    create basic map in map.html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let map;
+
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 41.87313493952422, lng: -87.62777109948178 },
+    zoom: 10.5,
+    mapId: "4504f8b37365c3d0",
+  });
+
+}
+
+initMap();
