85256f1bcdcd50906c59e19abe9430fa213eab72
jjoh1 <jjoh1@cps.edu>
2024-11-12 23:52:20 -0600

    remove marker color

--- js/script.js
+++ js/script.js
@@ -6,10 +6,6 @@ async function initMap() {
 
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  const pinBackground = new PinElement({
-    background: "#ffcfe6",
-  });
-
   map = new Map(document.getElementById("map"), {
     center: { lat: 41.87313493952422, lng: -87.62777109948178 },
     zoom: 10.5,
@@ -20,21 +16,18 @@ async function initMap() {
     map,
     position: { lat: 41.87313493952422, lng: -87.62777109948178 },
     title: "Jones College Prep",
-    content: pinBackground.element,
   });
   
   const loyola = new AdvancedMarkerElement({
     map,
     position: { lat: 41.99854414314861, lng: -87.65706614918372 },
     title: "Loyola University Chicago",
-    content: pinBackground.element,
   });
   
   const iit = new AdvancedMarkerElement({
     map,
     position: { lat: 41.85106289143754, lng: -87.6227089523229 },
     title: "Illinois Institute of Technology",
-    content: pinBackground.element,
   });
 }
 
